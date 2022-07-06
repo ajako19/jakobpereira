d0827667d86faa696a4a6a07aa46af682924230c
Jakob Pereira <jakepereira2004@gmail.com>
2022-07-06 12:30:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
+import Introduction from './components/Introduction';
+import MySkills from './components/MySkills';
+import Project from './components/Projects';
+import Aos from 'aos';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Introduction/>
+      <AboutMe/>
+      <MySkills/>
+      <Project/>
+      <Contact/>
     </div>
   );
 }
